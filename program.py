25c56bbba984b204c00b5ef6eeb5ff98ee800c75
heyhenry <henryyy3@hotmail.com>
2022-07-20 23:48:36 +0800

    Added mulitply and divide options

--- program.py
+++ program.py
@@ -4,7 +4,8 @@ print("A Simple Calculator")
 
 print("[1] Add")
 print("[2] Subtract")
-
+print("[3] Multiply")
+print("[4] Divide")
 option = int(input("Choose an option."))
 
 print(option)
@@ -13,6 +14,10 @@ if option == 1:
    print("time to add!")
 elif option == 2: 
    print("time to subtract")
+elif option == 3: 
+    print("time to multiply")
+elif option == 4: 
+    print("time to divide")
 else: 
    print("enter 1 or 2!")
 
