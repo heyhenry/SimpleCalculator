3a4c75ee41aec7a71f609e9ff4cf5925b691c706
heyhenry <henryyy3@hotmail.com>
2022-07-21 00:38:49 +0800

    Removed redundnat code.

--- program.py
+++ program.py
@@ -1,23 +1,3 @@
-# option = 0
-
-# print("A Simple Calculator")
-
-# print("[1] Add")
-# print("[2] Subtract")
-# print("[3] Multiply")
-# print("[4] Divide")
-# option = int(input("Choose an option."))
-
-# if option == 1:
-#    print("time to add!")
-# elif option == 2: 
-#    print("time to subtract")
-# elif option == 3: 
-#     print("time to multiply")
-# elif option == 4: 
-#     print("time to divide")
-# else: 
-#    print("enter 1 or 2!")
 
 running = True
 
